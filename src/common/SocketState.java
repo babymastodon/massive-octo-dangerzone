b737cfc8f2479fc659f65517a12a1b77086a08bf
Zack Drach <hogbait@gmail.com>
2013-12-01 18:24:25 -0500

    implemented SessionHandler

--- src/common/SocketState.java
+++ src/common/SocketState.java
@@ -1,10 +1,10 @@
 package common;
 
-enum SocketState {
+public enum SocketState {
     NOT_LOGGED_IN,
     LOGIN_PENDING,
     NOT_CONNECTED,
     CONNECT_PENDING,
     CONNECTED,
-    DISCONNECT_PENDING
+    DISCONNECT_PENDING;
 }
--- src/server/WhiteboardStruct.java
+++ src/server/WhiteboardStruct.java
@@ -9,15 +9,18 @@ import common.Whiteboard;
  * An object containing a final Whiteboard object, a list of ServerMessageListeners
  * listening on the whiteboard, and the ID number of the whiteboard.
  * 
- * It is NOT thread-safe.
+ * It is NOT thread-safe. Clients should lock this object before accessing
+ * or modifying any of its attributes.
  */
 public class WhiteboardStruct {
     private Whiteboard board;
     private List<ServerMessageListener> listeners;
+    private List<String> users;
     private int id;
     /**
      * Contruct with the given variables
      * @param board: the whiteboard that has all the drawing data
+     * @param users: list of users connected to the board
      * @param listeners: list of listeners for the board
      * @param id: the id number of the board.
      */
@@ -32,6 +35,13 @@ public class WhiteboardStruct {
      */
     public Whiteboard getWhiteboard(){
         return this.board;
+    }
+
+    /**
+     * @return a reference to the *mutable* list of usernames
+     */
+    public List<String> getUsers(){
+        return this.users;
     }
 
     /**
