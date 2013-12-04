09dcb47a2ee78b81b8ee27929bf51527c64ed1df
Zack Drach <hogbait@gmail.com>
2013-12-04 13:02:26 -0500

    fix whitespace

--- src/tests/common/ColorTest.java
+++ src/tests/common/ColorTest.java
@@ -9,33 +9,33 @@ import common.Color;
 
 public class ColorTest {
 
-	/**
-	 * Color tests
-	 */
-	
-	/**
-	 * Test color construction and methods.
-	 */
-	@Test
-	public void testConstructor(){
-      Color p = new Color(1,2,3);
-      assertEquals(1, p.getRed());
-      assertEquals(2, p.getGreen());
-      assertEquals(3, p.getBlue());
-	}
+    /**
+     * Color tests
+     */
+
+    /**
+     * Test color construction and methods.
+     */
+    @Test
+    public void testConstructor(){
+        Color p = new Color(1,2,3);
+        assertEquals(1, p.getRed());
+        assertEquals(2, p.getGreen());
+        assertEquals(3, p.getBlue());
+    }
 
 
-	/**
-	 * Test color equality and hashcode.
-	 */
-	@Test
-	public void testEquals(){
-      Color p1 = new Color(1,2,3);
-      Color p2 = new Color(1,2,3);
-      Color p3 = new Color(4,5,6);
-      assertEquals(true, p1.equals(p2));
-      assertEquals(false, p1.equals(p3));
-      assertEquals(false, p1.equals(""));
-      assertEquals(p1.hashCode(), p2.hashCode());
-	}
+    /**
+     * Test color equality and hashcode.
+     */
+    @Test
+    public void testEquals(){
+        Color p1 = new Color(1,2,3);
+        Color p2 = new Color(1,2,3);
+        Color p3 = new Color(4,5,6);
+        assertEquals(true, p1.equals(p2));
+        assertEquals(false, p1.equals(p3));
+        assertEquals(false, p1.equals(""));
+        assertEquals(p1.hashCode(), p2.hashCode());
+    }
 }
--- src/tests/common/PointTest.java
+++ src/tests/common/PointTest.java
@@ -9,32 +9,32 @@ import common.Point;
 
 public class PointTest {
 
-	/**
-	 * Point tests
-	 */
-	
-	/**
-	 * Test point construction and methods.
-	 */
-	@Test
-	public void testConstructor(){
-      Point p = new Point(1,2);
-      assertEquals(1, p.getX());
-      assertEquals(2, p.getY());
-	}
+    /**
+     * Point tests
+     */
+
+    /**
+     * Test point construction and methods.
+     */
+    @Test
+    public void testConstructor(){
+        Point p = new Point(1,2);
+        assertEquals(1, p.getX());
+        assertEquals(2, p.getY());
+    }
 
 
-	/**
-	 * Test point equality and hashcode.
-	 */
-	@Test
-	public void testEquals(){
-      Point p1 = new Point(1,2);
-      Point p2 = new Point(1,2);
-      Point p3 = new Point(1,4);
-      assertEquals(true, p1.equals(p2));
-      assertEquals(false, p1.equals(p3));
-      assertEquals(false, p1.equals(""));
-      assertEquals(p1.hashCode(), p2.hashCode());
-	}
+    /**
+     * Test point equality and hashcode.
+     */
+    @Test
+    public void testEquals(){
+        Point p1 = new Point(1,2);
+        Point p2 = new Point(1,2);
+        Point p3 = new Point(1,4);
+        assertEquals(true, p1.equals(p2));
+        assertEquals(false, p1.equals(p3));
+        assertEquals(false, p1.equals(""));
+        assertEquals(p1.hashCode(), p2.hashCode());
+    }
 }
