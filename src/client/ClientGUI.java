fd002444124cbc5ba5a08ffafb9e1783781d13ba
peter828 <peter828@mit.edu>
2013-12-08 18:27:52 -0500

    basicGUI implemented

--- src/client/ClientGUI.java
+++ src/client/ClientGUI.java
@@ -1,7 +1,24 @@
 package client;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+import canvas.Canvas;
 
 import common.*;
 
@@ -19,90 +36,184 @@ import common.*;
  * Instead of throwing exceptions, call serverClose()
  */
 public class ClientGUI implements ServerMessageListener{
-    private ArrayList<String> users = new ArrayList<String>();
-    private Whiteboard board = null;
-    private int boardID = -1;
-    private boolean loggedIn = false;
-    
-    
-    private ClientMessageListener cmListener = null;
+	private ArrayList<String> users = new ArrayList<String>();
+	private Whiteboard board = null;
+	private int boardID = -1;
+	private boolean loggedIn = false;
 
-    
-    /**
-     * Use the provided listener object to send messages to the server.
-     * @param l: the listener used to send messages.
-     */
-    public void setClientMessageListener(ClientMessageListener l){
-        assert this.cmListener == null;
-        this.cmListener = l;
-    }
 
-    /**
-     * Display the GUI on screen, and start listening for events
-     * from the user.
-     *
-     * precondition: setClientMessageListener must have been called
-     * with a non-null argument.
-     */
-    public void start(){
-        assert cmListener != null;
-        
-        //TODO display the gui...
-    }
+	private ClientMessageListener cmListener = null;
 
-    @Override
-    public void loginSuccess() {
-        // TODO Auto-generated method stub
-        this.loggedIn = true;
-    }
 
-    @Override
-    public void error(int code) {
-        //TODO: display these in the gui
-        System.err.println("Error number " + code);
-        if (code == 100){
-            System.err.println("The username was already taken");
-        }
-        else if (code == 200){
-            System.err.println("There is no board with that id");
-        }
-        else{
-            System.err.println("Unrecognized error");
-        }
-    }
+	/**
+	 * Use the provided listener object to send messages to the server.
+	 * @param l: the listener used to send messages.
+	 */
+	public void setClientMessageListener(ClientMessageListener l){
+		assert this.cmListener == null;
+		this.cmListener = l;
+	}
 
-    @Override
-    public void connectToBoardSuccess(int id, List<String> users,
-            Whiteboard data) {
-        // TODO change gui to see the board.
-        this.users = new ArrayList<String>(users);
-        this.board = data;
-        this.boardID = id;
-    }
+	/**
+	 * Display the GUI on screen, and start listening for events
+	 * from the user.
+	 *
+	 * precondition: setClientMessageListener must have been called
+	 * with a non-null argument.
+	 */
+	public void start(){
+		assert cmListener != null;
+		// set up the UI (on the event-handling thread)
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				final Canvas canvas = new Canvas(800, 600);
+				JFrame window = new JFrame("Whiteboard");
+				window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				//penSize=5;
 
-    @Override
-    public void updatePixel(Point point, Color color) {
-        this.board.setPixel(point, color);
-    }
+				JButton eraseButton = new JButton("ERASE");
+				eraseButton.setName("ERASE");
+				eraseButton.setPreferredSize(new Dimension (100,100));
+				eraseButton.addActionListener( new ActionListener(){
+					public void actionPerformed(ActionEvent e) {
+						canvas.setColor(Color.white);
+						canvas.setPenSize(30);
+					}
+				});
+				//TODO String users should be concatenated with users info. 
+				String Users;
+				JLabel users=new JLabel("Current Users: ");
+				JLabel colorInstr=new JLabel("Choose your color");
 
-    @Override
-    public void updateUsers(List<String> users) {
-        this.users = new ArrayList<String>(users);
-    }
 
-    @Override
-    public void disconnectFromBoardSuccess() {
-        this.board = null;
-        this.boardID = -1;
-        this.users = new ArrayList<String>();
-    }
+				JButton blackButton = new JButton("BLACK");
+				blackButton.setName("BLACK");
+				blackButton.setPreferredSize(new Dimension (100,100));
+				blackButton.addActionListener( new ActionListener(){
+					public void actionPerformed(ActionEvent e) {
+						canvas.setColor(Color.black);
+						canvas.setPenSize(5);
+					}
+				});
 
-    @Override
-    public void serverClose() {
-        this.board = null;
-        this.boardID = -1;
-        this.users = new ArrayList<String>();
-        
-        cmListener.clientClose();
-    }
+				JButton redButton = new JButton("RED");
+				redButton.setName("RED");
+				redButton.setPreferredSize(new Dimension (100,100));
+				redButton.addActionListener( new ActionListener(){
+					public void actionPerformed(ActionEvent e) {
+						canvas.setColor(Color.red);
+						canvas.setPenSize(5);
+					}
+				});
+
+				JButton blueButton = new JButton("BLUE");
+				blueButton.setName("BLUE");
+				blueButton.setPreferredSize(new Dimension (100,100));
+				blueButton.addActionListener( new ActionListener(){
+					public void actionPerformed(ActionEvent e) {
+						canvas.setColor(Color.blue);
+						canvas.setPenSize(5);
+					}
+				});
+
+				JButton greenButton = new JButton("GREEN");
+				greenButton.setName("GREEN");
+				greenButton.setPreferredSize(new Dimension (100,100));
+				greenButton.addActionListener( new ActionListener(){
+					public void actionPerformed(ActionEvent e) {
+						canvas.setColor(Color.green);
+						canvas.setPenSize(5);
+					}
+				});
+				
+				JButton exitButton = new JButton("EXIT");
+				exitButton.setName("EXIT");
+				exitButton.setPreferredSize(new Dimension (100,100));
+				exitButton.addActionListener( new ActionListener(){
+					public void actionPerformed(ActionEvent e) {
+						//color=Color.BLACK;
+						//penSize=5;
+					}
+				});
+				JPanel canvasWithUsers=new JPanel();
+				canvasWithUsers.setLayout(new BoxLayout(canvasWithUsers, BoxLayout.Y_AXIS));
+				canvasWithUsers.add(canvas);
+				canvasWithUsers.add(users);
+		
+				JPanel drawAndErase=new JPanel();
+				drawAndErase.setLayout(new BoxLayout(drawAndErase, BoxLayout.Y_AXIS));
+				drawAndErase.add(colorInstr);
+				drawAndErase.add(blackButton);
+				drawAndErase.add(redButton);
+				drawAndErase.add(blueButton);
+				drawAndErase.add(greenButton);
+				drawAndErase.add(eraseButton);
+				drawAndErase.add(exitButton);
+				
+				window.setLayout(new FlowLayout());
+				window.add(canvasWithUsers);
+				window.add(drawAndErase);
+				window.pack();
+				window.setVisible(true);
+			}
+		});
+
+		//TODO display the gui...
+	}
+
+	@Override
+	public void loginSuccess() {
+		// TODO Auto-generated method stub
+		this.loggedIn = true;
+	}
+
+	@Override
+	public void error(int code) {
+		//TODO: display these in the gui
+		System.err.println("Error number " + code);
+		if (code == 100){
+			System.err.println("The username was already taken");
+		}
+		else if (code == 200){
+			System.err.println("There is no board with that id");
+		}
+		else{
+			System.err.println("Unrecognized error");
+		}
+	}
+
+	@Override
+	public void connectToBoardSuccess(int id, List<String> users,
+			Whiteboard data) {
+		// TODO change gui to see the board.
+		this.users = new ArrayList<String>(users);
+		this.board = data;
+		this.boardID = id;
+	}
+
+	@Override
+	public void updatePixel(Point point, Color color) {
+		this.board.setPixel(point, color);
+	}
+
+	@Override
+	public void updateUsers(List<String> users) {
+		this.users = new ArrayList<String>(users);
+	}
+
+	@Override
+	public void disconnectFromBoardSuccess() {
+		this.board = null;
+		this.boardID = -1;
+		this.users = new ArrayList<String>();
+	}
+
+	@Override
+	public void serverClose() {
+		this.board = null;
+		this.boardID = -1;
+		this.users = new ArrayList<String>();
+
+		cmListener.clientClose();
+	}
 }
